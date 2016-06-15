6a56e0c23b13f89f565b19a6bafb5a50afdbefd3
aureliome <aurelio.me@gmail.com>
2016-06-15 16:17:05 +0200

    Version 1.0.0, bower.json and package.json completed

---
+++ 1.0.0/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "NewtonAdapter",
+      "shortName": "NewtonAdapter",
+      "type": "object",
+      "moduleName": "NewtonAdapter",
+      "shortDescription": "Adapter for Newton sdk to be used in B! web applications",
+      "keywords": "adapter allowed application applications category completed configuration console content contentid custom data debug details enable enabled error event events external freemium fruit function game getsharedinstancewithconfig google href http info init initializes internal isuserlogged location log logged logger login mandatory method methods msisdn newton newtonadapter object options pageview performs play playing product promise properties rank resolved returns scope score sdk secret secretid sets slicer social startheartbeat stopheartbeat stops timed title track trackevent tracking trackpageview true type url user userid userproperties values version wait waitlogin warn web"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/NewtonAdapter",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
