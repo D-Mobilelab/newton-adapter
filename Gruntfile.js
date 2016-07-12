1621cf5da6be0428a913e7fe18ff10549248f97e
aureliome <aurelio.me@gmail.com>
2016-07-12 11:00:54 +0200

    Test for Grunt travis /7

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function (grunt) {
     ****         REQUIREMENTS         ****
     *************************************/
 
-    grunt.loadNpmTasks('./node_modules/dml-js-boilerplate/node_modules/grunt-contrib-clean/tasks');
-    grunt.loadTasks('./node_modules/dml-js-boilerplate/node_modules/grunt-contrib-connect/tasks');
+    // grunt.loadNpmTasks('./node_modules/dml-js-boilerplate/node_modules/grunt-contrib-clean/tasks');
+    // grunt.loadTasks('./node_modules/dml-js-boilerplate/node_modules/grunt-contrib-connect/tasks');
     require('load-grunt-tasks')(grunt, {
     	config: './node_modules/dml-js-boilerplate/package.json', 
     	scope: 'dependencies',
