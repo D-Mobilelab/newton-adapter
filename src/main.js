ad31fba8ae2b124a765bb3fbab63a4d8c0798b69
eatsjobs <pasqualemangialavori@buongiorno.com>
2017-04-10 16:09:24 +0200

    add pushCallback in options

--- src/main.js
+++ src/main.js
@@ -58,6 +58,7 @@ var NewtonAdapter = new function(){
     * @param {integer} [options.newtonversion=2] version of Newton (1 or 2)
     * @param {Object} [options.logger=disabled logger] object with debug, log, info, warn, error
     * @param {Object} [options.properties={}] custom data for Newton session (not supported for v1)
+    * @param {Function} [options.pushCallback=null] a function that will be called in hybrid init(wait for device ready must be true)
     *
     * @return {Promise} promise will be resolved when init is completed, rejected if failed
     * 
@@ -72,7 +73,8 @@ var NewtonAdapter = new function(){
     *       logger: console,
     *       properties: {
     *           hello: 'World'
-    *       }
+    *       },
+    *       pushCallback:function(pushData){}
     *   }).then(function(enabled){
     *       console.log('init success', enabled);
     *   }).catch(function(err){
@@ -104,7 +106,7 @@ var NewtonAdapter = new function(){
                             logger.warn('NewtonAdapter', 'Init', 'Newton v.1 not support properties on init method');
                         }
                     } else {
-                        newtonInstance = Newton.getSharedInstanceWithConfig(options.secretId, createSimpleObject(options.properties));
+                        newtonInstance = Newton.getSharedInstanceWithConfig(options.secretId, createSimpleObject(options.properties), options.pushCallback);
                     }
 
                     // trigger init
