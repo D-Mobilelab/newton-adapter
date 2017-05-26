6111c875c2fd20854b9d12b7b3f5aec0af2ec87f
gianni <fiorentinig@gmail.com>
2017-05-26 16:26:12 +0200

    added autologin method

--- src/main.js
+++ src/main.js
@@ -733,6 +733,43 @@ var NewtonAdapter = new function(){
 
     /**
     * @ngdoc function
+    * @name autoLogin
+    * @methodOf NewtonAdapter
+    *
+    * @description start autoLogin flow
+    *
+    * @param {Object} options configuration object
+    * @param {string} options.waitingUrl url for waiting page
+    * @param {string} options.subscribeUrl url for account page (redirect here if user isn't subscribed)
+    *    *
+    * @example
+    * <pre>
+    *   NewtonAdapter.autoLogin({
+    *     "waitingUrl":"http://www.domain.com/#!/waiting",
+          "subscribeUrl":"http://www.domain.com/#!/settings"
+    *   });
+    * </pre>
+    */
+    this.autoLogin = function(options){
+        var instance;
+        if(options.waitingUrl){
+
+            instance = newtonInstance.getLoginBuilder();
+            if(options.subscribeUrl) {
+                instance.setSubscribeUrl(options.subscribeUrl);
+            }
+
+            instance.setWaitingUrl(options.waitingUrl)
+            .getMSISDNURLoginFlow()
+            .startLoginFlow();
+
+        } else {
+            logger.error('NewtonAdapter', 'autoLogin', 'autoLogin requires waiting url');
+        }
+    };
+
+    /**
+    * @ngdoc function
     * @name confirmEmailAndLogin
     * @methodOf NewtonAdapter
     *
