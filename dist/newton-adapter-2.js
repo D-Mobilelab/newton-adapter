27b075eafa9525ee27167ecfb115cbf2214a30ec
Pasquale Mangialavori <pasqualemangialavori@docomodigital.com>
2018-03-29 10:26:13 +0200

    version 2.14.2

--- dist/newton-adapter-2.js
+++ dist/newton-adapter-2.js
@@ -373,7 +373,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	  }
 
 	  function Promise(fn) {
-	    if (typeof this !== 'object') throw new TypeError('Promises must be constructed via new');
+	    if (!(this instanceof Promise)) throw new TypeError('Promises must be constructed via new');
 	    if (typeof fn !== 'function') throw new TypeError('not a function');
 	    this._state = 0;
 	    this._handled = false;
@@ -497,9 +497,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 
 	  Promise.all = function (arr) {
-	    var args = Array.prototype.slice.call(arr);
-
 	    return new Promise(function (resolve, reject) {
+	      if (!arr || typeof arr.length === 'undefined') throw new TypeError('Promise.all accepts an array');
+	      var args = Array.prototype.slice.call(arr);
 	      if (args.length === 0) return resolve([]);
 	      var remaining = args.length;
 
@@ -597,7 +597,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	var apply = Function.prototype.apply;
+	/* WEBPACK VAR INJECTION */(function(global) {var apply = Function.prototype.apply;
 
 	// DOM APIs, for completeness
 
@@ -648,9 +648,17 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	// setimmediate attaches itself to the global object
 	__webpack_require__(6);
-	exports.setImmediate = setImmediate;
-	exports.clearImmediate = clearImmediate;
+	// On some exotic environments, it's not clear which object `setimmeidate` was
+	// able to install onto.  Search each possibility in the same order as the
+	// `setimmediate` library.
+	exports.setImmediate = (typeof self !== "undefined" && self.setImmediate) ||
+	                       (typeof global !== "undefined" && global.setImmediate) ||
+	                       (this && this.setImmediate);
+	exports.clearImmediate = (typeof self !== "undefined" && self.clearImmediate) ||
+	                         (typeof global !== "undefined" && global.clearImmediate) ||
+	                         (this && this.clearImmediate);
 
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ }),
 /* 6 */
@@ -2013,14 +2021,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	*
 	* @example
 	* <pre>
-	* NewtonAdapter.setUserStateChangeListener(function(state){ console.log(state); });
+	* NewtonAdapter.setUserStateChangeListener({ onLoginStateChange: function(state){ ... }});
 	* </pre>
 	*/
-	module.exports = function(callback){
-	    if(Global.newtonInstance && callback){
-	        Global.newtonInstance.setUserStateChangeListener({
-	            onLoginStateChange: callback
-	        });        
+	module.exports = function(objCallbacks){
+	    if(Global.newtonInstance && objCallbacks) {
+	        Global.newtonInstance.setUserStateChangeListener(objCallbacks);        
 	        return true;
 	    } else {
 	        return false;
@@ -2740,4 +2746,4 @@ return /******/ (function(modules) { // webpackBootstrap
 });
 ;
 
-/* Newton Adapter 2.14.1 */
+/* Newton Adapter 2.14.2 */
